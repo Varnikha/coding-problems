0af5b28b580559432cb33bb8fd66dd0264f57f5b
varnikha varshini <141479119+Varnikha@users.noreply.github.com>
2025-06-18 11:36:53 +0530

    Create missing-number.py

---
+++ Problems/Array/missing-number.py
@@ -0,0 +1,20 @@
+Problem: Missing Number
+Platform: LeetCode #268
+Difficulty: Easy
+Date Solved: 2024-06-18
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Problem Description:
+Given an array nums containing n distinct numbers in the range [0, n], 
+return the only number in the range that is missing from the array.
+
+Solution 1:
+
+class Solution(object):
+    def missingNumber(self, nums):
+        res=len(nums)
+        for i in range (len(nums)):
+            res+=i
+            res-=nums[i]
+        return res
